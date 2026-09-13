d543ed2b1da6beae47404c8ff15a7e5017b26890
CarlosCostaMath <carlosmatfis@gmail.com>
2026-09-13 17:32:00 -0300

    fix: route search indexing through Python

--- bookml-perl-wrapper.pl
+++ bookml-perl-wrapper.pl
@@ -5,7 +5,9 @@ use warnings;
 my @args = @ARGV;
 
 if (@args && $args[0] eq 'bookml/search_index.pl') {
-  $args[0] = 'bookml-search-index.pl';
+  shift @args;
+  exec 'python3', 'bookml-search-index.py', @args
+    or die "cannot execute Python search indexer: $!";
 }
 
 exec $^X, @args or die "cannot execute Perl command: $!";
